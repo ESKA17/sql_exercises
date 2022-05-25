2ced7809724aff21747ad8bb3bddbce97fff554c
Saba <sabakurmashev@gmail.com>
2022-05-25 12:11:45 +0600

    small change

--- ex_03.sql
+++ ex_03.sql
@@ -1,5 +1,1 @@
--- 3
-SELECT c.FirstName || ' ' || c.Lastname AS FullName, i.InvoiceId, i.InvoiceDate, i.BillingCountry
-FROM Customer c
-         JOIN Invoice i ON c.CustomerId = i.CustomerId
-WHERE Country = 'Brazil';
+select firstname ||  ' ' || lastname as FullName, invoiceid, invoicedate, billingcountry from customer join invoice using (customerid) where Country = 'Brazil';
